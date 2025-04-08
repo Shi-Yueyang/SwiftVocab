8997699bf863d870b390670eac9b6ead0f5bff7e
syy <syy319@sina.com>
2025-04-08 15:22:53 +0800

    delete pics, bug fix

--- miniprogram/pages/settings/settings.ts
+++ miniprogram/pages/settings/settings.ts
@@ -1,45 +1,46 @@
-
-
 Page({
   data: {
-    buttons: [{text: '取消'}, {text: '确认'}],
-    totalWords:0,
+    buttons: [{ text: "取消" }, { text: "确认" }],
+    totalWords: 0,
     learningProgress: [] as LearningProgress[],
-    allSources: [] as Source[]
-
+    allSources: [] as Source[],
   },
 
   onLoad() {
-    console.log('Settings page loaded');
-    this.LoadProgress();
-    
+    console.log("Settings page loaded");
   },
-  
+  onShow() {
+    console.log("Settings page shown");
+    this.LoadProgress();
+  },
+
   async LoadProgress() {
+    // set learning progress
     const app = getApp<IAppOption>();
-    this.setData({
-      totalWords: app.globalData.userExperience?.totalWords || 0 // for display
-    });
-
     const db = wx.cloud.database();
-    const progressData = await db.collection('userProgress').get()
-
+    const progressData = await db.collection("userProgress").get();
     const learningProgress = progressData.data as LearningProgress[];
     const learningPrograsWithTotal = learningProgress.map((item) => {
-      const source = app.globalData.allSources.find((source) => source.name === item.source);
+      const source = app.globalData.allSources.find(
+        (source) => source.name === item.source
+      );
+      if (!source) {
+        return item; // Return the original item if source is not found
+      }
       return {
         ...item,
-        totalNumber: source ? source.count : 0,
-        progressPercent: (item.finalIndex / (source ? source.count : 1))*100 , // Avoid division by zero
+        totalNumber: source.count,
+        progressPercent: (item.finalIndex / source.count) * 100, // Avoid division by zero
       };
     });
     this.setData({
       learningProgress: learningPrograsWithTotal,
     });
-    const userExperienceData = await db.collection('userExperience').get()
-    this.setData({
-      totalWords: userExperienceData.data[0].totalWords,  
-    });
 
-  }
-})
+    // set user experience
+    const userExperienceData = await db.collection("userExperience").get();
+    this.setData({
+      totalWords: userExperienceData.data[0].totalWords,
+    });
+  },
+});
